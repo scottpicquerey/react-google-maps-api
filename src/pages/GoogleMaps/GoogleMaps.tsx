53c8c3f92eaedccf8acba002f707f1b6a6c777c5
Scott Picquerey <scott.picquerey@inato.com>
2023-04-27 18:35:20 +0200

    Display the map

--- src/pages/GoogleMaps/GoogleMaps.tsx
+++ src/pages/GoogleMaps/GoogleMaps.tsx
@@ -1,3 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-export const GoogleMaps = () => <div>GoogleMaps</div>;
+const DEFAULT_CENTER = { lat: 48.8566, lng: 2.3522 };
+const DEFAULT_ZOOM = 7;
+
+export const GoogleMaps = ({ className }: { className?: string }) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    // Display the map
+    if (ref.current) {
+      new window.google.maps.Map(ref.current, {
+        center: DEFAULT_CENTER,
+        zoom: DEFAULT_ZOOM,
+      });
+    }
+  }, [ref]);
+
+  return (
+    <div
+      className={className}
+      ref={ref}
+      style={{ width: "1000px", height: "700px" }}
+    />
+  );
+};
